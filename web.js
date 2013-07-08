03f0817735cf092805643953bcd1ce07e3afbbb4
oilpig <aoyuzhang1989@gmail.com>
2013-07-08 16:29:20 +0000

    updated web HW3

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var buffer =  new Buffer(20);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World lzw!');
+  	buffer = fs.readFileSync(index.html);
+
+	response.send(buffer.toString("buffer"));
 });
 
 var port = process.env.PORT || 5000;
